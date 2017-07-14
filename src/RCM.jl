30dc774ff99d2d6000c5c3bbac2d1b3fdd9525ae
Jukka Aho <ahojukka5@gmail.com>
2017-07-14 16:59:01 +0300

    Update RCM.jl
    Some minor modifications to code.

--- src/RCM.jl
+++ src/RCM.jl
@@ -2,38 +2,32 @@
 # License is MIT: see https://github.com/JuliaFEM/NodeNumbering.jl/blob/master/LICENSE.md
 
 """
-    RCM(adjacency::Dict{Int, Vector{Int}}, degrees::Dict{Int, Int}, P)
+    RCM(adjacency, degrees, P)
 
 Calculate the Reverse Cuthill-McKee Algorithm for the adjacency graph.
 
-Parameters
-----------
+# Parameters
 adjacency = A Dict where the keys are node numbers and the values are lists of adjacent nodes.
 degrees = A Dict where the keys are node numbers and the values are degrees of the nodes.
 P = The starting node for the algorithm. It is one of the lowest degree nodes in the graph.
 
-Returns
--------
-neworder = A list where the nodes are ordered with the RCM Algorithm.
+# Returns
+new_order = A list where the nodes are ordered with the RCM Algorithm.
 
-Notes
------
+# References
 [1] https://en.wikipedia.org/wiki/Cuthill%E2%80%93McKee_algorithm
 """
-function RCM(adjacency::Dict{Int, Vector{Int}}, degrees::Dict{Int, Int}, P)
-    n=length(adjacency)
-    R = Int[] #Creating the CM-ordered list
-    push!(R, P) #Adding parent to the result list
+function RCM(adjacency::Dict{Int, Vector{Int}}, degrees::Dict{Int, Int}, P::Int)::Vector{Int}
+    n = length(adjacency)
+    R = Int[P]  # Creating the CM-ordered list
     for i=1:n
-        t=adjacency[R[i]]
-        sort!(t, by=j->degrees[j])
+        t = sort(adjacency[R[i]], by=j->degrees[j])
         for T in t
-          if in(T, R) == false
-            push!(R, T)
-          end
+            if !(T in R)
+                push!(R, T)
+            end
         end
     end
-    println(R)
-    neworder = reverse(R)
-    return neworder
+    new_order = reverse(R)
+    return new_order
 end
