18cc2454992ee5ff9d33c57663b92256a326a3f4
Jukka Aho <ahojukka5@gmail.com>
2017-07-15 11:29:33 +0300

    Generate documentation for project automatically (#16)
    * documentation set up using Documenter.jl
    * calculate_bandwidth(): fix docstring to give a good example how it should be done
    * initial .gitignore
    * setup documentation hosting, pages should be to juliafem.github.io/NodeNumbering.jl
    * closes #12

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+# This file is a part of project JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/NodeNumbering.jl/blob/master/LICENSE
+
+using Documenter
+using NodeNumbering
+
+makedocs(
+    modules = [NodeNumbering]
+)
+
+deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/JuliaFEM/NodeNumbering.jl.git"
+)
--- src/calc_bw.jl
+++ src/calc_bw.jl
@@ -2,19 +2,31 @@
 # License is MIT: see https://github.com/JuliaFEM/NodeNumbering.jl/blob/master/LICENSE
 
 """
-Calculate bandwidth of adjacency graph G.
+    calculate_bandwidth(G::Dict{Int, Vector{Int}})
 
-Parameters
-----------
-G   Dict; key is node number and value is a list of adjacent nodes
+Calculate bandwidth of graph ``G``. The bandwidth of the matrix is number ``k``
+such that ``A_{ij} = 0`` if ``|i-j| > k``.
 
-Returns
--------
-Integer
+# Examples
 
-Notes
------
-[1] https://en.wikipedia.org/wiki/Band_matrix
+```jldoctest
+julia> G = Dict(
+           1 => [3, 8, 9],
+           2 => [3, 8, 7],
+           3 => [1, 2],
+           4 => [8, 9],
+           5 => [7, 8],
+           6 => [2, 7],
+           7 => [5, 2, 6],
+           8 => [1, 2, 4, 5],
+           9 => [1, 4])
+julia> calculate_bandwidth(G)
+17
+```
+
+# References
+
+* Wikipedia contributors. "Band matrix." Wikipedia, The Free Encyclopedia. Wikipedia, The Free Encyclopedia, 28 Apr. 2017. Web. 15 Jul. 2017. https://en.wikipedia.org/wiki/Band_matrix.
 """
 function calculate_bandwidth(G::Dict{Int, Vector{Int}})
     return 2*maximum([maximum(j-g) for (j,g) in G]) + 1
